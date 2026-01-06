260b76a84dc17969f633819b8080427068e44e20
Luka Vukovic <vukovicluka227@gmail.com>
2026-01-06 11:50:04 +0100

    feat: add initial navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        About screen
+      </Link>
     </View>
   );
 }
@@ -13,9 +17,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   text: {
-    color: "#fff",
+    color: "#fff"
   },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff"
+  }
 });
