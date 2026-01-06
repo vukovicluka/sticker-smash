e57b8a7644e3fbc555a0e9f960270203083b2260
Luka Vukovic <vukovicluka227@gmail.com>
2026-01-06 13:12:05 +0100

    feat: add an image picker

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -3,16 +3,19 @@ import { Image } from "expo-image";
 
 type Props = {
   imgSource: ImageSourcePropType;
+  selectedImage?: string;
 };
 
-export default function ImageViewer({ imgSource }: Props) {
-  return <Image source={imgSource} style={styles.image} />;
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
+  return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
   image: {
     width: 320,
     height: 440,
-    borderRadius: 18,
-  },
+    borderRadius: 18
+  }
 });
